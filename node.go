e2b9ded56fba70a072c000d8b2613bcdd0d03c26
Tudor Hulban <prindex2010@gmail.com>
2022-03-01 15:44:42 +0200

    start-node

--- node.go
+++ node.go
@@ -1,7 +1,10 @@
 package fingers
 
 import (
+	"fmt"
 	"hash"
+	"net"
+	"net/rpc"
 	"sync"
 	"time"
 )
@@ -33,6 +36,47 @@ type Node struct {
 	storage     store
 	transport   Transport
 
-	chShutdown    chan struct{}
+	chShutdown    chan struct{} // TODO: assess best type
 	lastStablized time.Time
+}
+
+func NewNode(cfg *Config) (*Node, error) {
+	// TODO: validation of config
+
+	return &Node{
+		chShutdown: make(chan struct{}),
+	}, nil
+}
+
+func (n *Node) Start(o *Operations) error {
+	rpc.Register(&o)
+
+	tcpAddr, errRes := net.ResolveTCPAddr("tcp", string(n.cfg.Socket))
+	if errRes != nil {
+		return errRes
+	}
+
+	listener, errLis := net.ListenTCP("tcp", tcpAddr)
+	if errLis != nil {
+		return errLis
+	}
+
+	go func() {
+		fmt.Printf("listening on port %s\n", n.cfg.Socket)
+
+		select {
+		case sign := <-n.chShutdown:
+			{
+				fmt.Printf("\nshutting node on received: %s.\n", sign)
+				close(n.chShutdown)
+			}
+
+		default:
+			{
+				rpc.Accept(listener)
+			}
+		}
+	}()
+
+	return nil
 }
---
+++ operation.go
@@ -0,0 +1,51 @@
+package fingers
+
+type Operations struct{}
+
+func (o Operations) GetSuccessor() (*Node, error) {
+	return nil, nil
+}
+
+func (o Operations) FindSuccessor([]byte) (*Node, error) {
+	return nil, nil
+}
+
+func (o Operations) GetPredecessor() (*Node, error) {
+	return nil, nil
+}
+
+func (o Operations) Notify(*Node) error {
+	return nil
+}
+
+func (o Operations) CheckPredecessor(*Node) error {
+	return nil
+}
+
+func (o Operations) SetPredecessor(*Node) error {
+	return nil
+}
+
+func (o Operations) SetSuccessor(succ *Node) error {
+	return nil
+}
+
+func (o Operations) GetKey(string) (*GetResponse, error) {
+
+}
+
+func (o Operations) SetKey(string, string) error {
+	return nil
+}
+
+func (o Operations) DeleteKey(string) error {
+	return nil
+}
+
+func (o Operations) RequestKeys([]byte, []byte) ([]*KV, error) {
+	return nil, nil
+}
+
+func (o Operations) DeleteKeys([]string) error {
+	return nil
+}
